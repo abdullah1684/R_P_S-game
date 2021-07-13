c2d3c1be22f5d83c634380774db20780c808d15c
abdullah1684 <74565690+abdullah1684@users.noreply.github.com>
2021-07-13 19:27:18 +0300

    Add files via upload

--- R_P_S.py
+++ R_P_S.py
@@ -1,14 +1,58 @@
-def f_emoji(massage):
-    emoji = {
-        ":)": "😁",
-        ":(": "🙁"
-    }
-    words = message.split()
-    output = ""
-    for word in words:
-        output += emoji.get(word, word) + " "
-    return output
+import random
 
-message = input(">")
-result = f_emoji(message)
-print(result)
+print("Welcome to R_P_S Game!")
+print("""
+Choose :
+'s' for Scissors
+'p' for Paper
+'r' for Rock
+""")
+
+list1 = ['s', 'p', 'r']
+computer = random.choice(list1)
+player = input('>').lower()
+
+while True:
+    if player == 's' and computer == 'r':
+        print("You Lose!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == 'r' and computer == 's':
+        print("You Won!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == 'p' and computer == 'r':
+        print("You Won!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == 'r' and computer == 'p':
+        print("You Lose!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == 's' and computer == 'p':
+        print("You Won!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == 'p' and computer == 's':
+        print("You Lose!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    elif player == computer:
+        print("It's DRAW!")
+        print(f"Computer choice is : {computer}")
+        break
+
+    else:
+        print("Invalid Input")
+        print(f"Computer choice is : {computer}")
+        break
+
+
+
+
